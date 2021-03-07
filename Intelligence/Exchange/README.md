72b0bd37187d64e0efd10f84d621fc90cba26ca1
unknown <ollie.whitehouse@nccgroup.com>
2021-03-07 08:54:02 +0000

    README update and added md5check.bat

--- Intelligence/Exchange/README.md
+++ Intelligence/Exchange/README.md
@@ -3,3 +3,4 @@ Generated from the installations of all files, generated using:
 * find /[PATHTOEXCHANGEINSTALLROOT]/Setup/ServerRoles/ -type f -exec sha1sum {} \; > SHA1
 * find /[PATHTOEXCHANGEINSTALLROOT]/Setup/ServerRoles/ -type f -exec sha256sum {} \; > SHA2
 
+We've also included md5check.bat which is able to use the MD5 hash sets to identify unexpected files.
---
+++ Intelligence/Exchange/md5check.bat
@@ -0,0 +1,52 @@
+@echo off
+
+REM °²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²°
+REM °² Calc file hashes and check they are present      ²°
+REM °² in a file                                        ²°
+REM °²                                                  ²°
+REM °² twitter: @ollieatnccgroup                        ²°
+REM °²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²°
+
+REM example usage
+REM md5check.bat "C:\Users\Ollie Whitehouse\Downloads\Exchange2013-x64-cu23\setup\MD5"
+
+REM Paramater checking
+set "param1=%~1"
+
+if "!param1!"=="" ( 
+    echo [i] Get hash set from https://github.com/nccgroup/Cyber-Defence/tree/master/Intelligence/Exchange
+    echo [i] then supply as paramater e.g. check.bat "C:\Path\to\MD5"
+    exit /b
+)
+
+if not exist "%param1%" (
+    echo "[i] %param1% does not exist"
+    exit /b
+)
+
+
+REM features++
+SETLOCAL ENABLEDELAYEDEXPANSION 
+
+call :treeProcess
+goto :eof
+
+:treeProcess
+rem Do whatever you want here over the files of this subdir, for example:
+for %%f in (*.*) do (
+    REM certutil -hashfile %%f MD5 | findstr /V ":"
+    for /f "delims=" %%A in ('certutil -hashfile %%f MD5 ^| findstr /V ":"') do (
+         set "var=%%A"
+         REM echo %%f %%A
+         findstr /I %%A %1
+	 if not %errorlevel% neq 0 echo [*] %%f with MD5 of %%A not found
+    )
+)
+
+for /D %%d in (*) do (
+    cd %%d
+    call :treeProcess
+    cd ..
+)
+
+exit /b
